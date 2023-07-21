b90045baa0d3856c9a5a56e5cdd2a1862c70a887
Basel Ayman <baselaymansalah@gmail.com>
2023-07-21 21:00:09 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -9,10 +9,10 @@ import requests
 import config
 import pickle
 import io
-import torch
-from torchvision import transforms
+# import torch
+# from torchvision import transforms
 from PIL import Image
-from utils.model import ResNet9
+# from utils.model import ResNet9
 # ==============================================================================================
 
 # -------------------------LOADING THE TRAINED MODELS -----------------------------------------------
